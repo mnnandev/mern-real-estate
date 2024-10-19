75545da3ff542c73c5131460ae8a214dfb812954
mnnandev <mnnanhavibhutta@gamil.com>
2024-10-19 07:16:36 -0700

    signIn page added ui

--- client/src/Components/SignIn.jsx
+++ client/src/Components/SignIn.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle sign-in logic here
+  };
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="border p-3 rounded-lg"
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="border p-3 rounded-lg"
+          required
+        />
+        <button
+          type="submit"
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          Sign In
+        </button>
+        <button
+          type="button"
+          className="bg-red-700 text-white rounded-lg p-3 uppercase hover:opacity-95"
+        >
+          Continue with Google
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Don't have an account?</p>
+        <Link to="/sign-up">
+          <span className="text-blue-500">Sign up</span>
+        </Link>
+      </div>
+      <p className="text-red-700 mt-5"></p>
+    </div>
+  );
 };
 
 export default SignIn;
