1e9a10272a0a16d6b96f9a3f7e2ee7bb5115d4ee
mnnandev <mnnanhavibhutta@gamil.com>
2024-10-21 07:31:58 -0700

    signup errors added

--- Api/controllers/auth.controller.js
+++ Api/controllers/auth.controller.js
@@ -21,7 +21,7 @@ export const SignIn = async (req, res, next) => {
     const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(404, "user not found"));
     const validpassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validpassword) return next(errorHandler(404, "Invalid password"));
+    if (!validpassword) return next(errorHandler(404, "wrong credentionals"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY);
     const { password: pas, ...rest } = validUser._doc; // Destructure the password out of the user object and keep the rest of the user data except password
     res
